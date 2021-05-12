c084beca7b2da370cb39133c2fa0e20ee2f7749a
Michele Ferro <mykael98@live.it>
2021-05-12 23:27:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,23 +20,35 @@ X = dataset.drop('objType', axis=1)
 print(X)
 print(y)
 
+# 80% dei dati per allenamento, 20% test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
 print(X_train)
 print(y_train)
 
+# training con decision tree
 print("DECISION TREE")
 classifier = DecisionTreeClassifier()
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
-print(accuracy_score(y_test, y_pred))
+print("Accuracy", accuracy_score(y_test, y_pred,))
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 print(y_pred)
 
+# prediction
+print("\t---RESULTS---")
 pr = pd.read_csv('mongialino_topredict.csv')
+names = pr['object']
 pr.drop('object', axis=1, inplace=True)
 pr.drop('collection', axis=1, inplace=True)
 pred = classifier.predict(pr)
-print(pred)
+
+pred_df = pd.DataFrame(pred)
+
+# result
+res = pd.concat([names, pred_df], axis=1)
+res.rename({ 0 : 'objType'}, axis=1, inplace=True)
+print(res)
+
 
